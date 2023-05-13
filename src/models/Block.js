172a8b7ef51fc9c20dc4765b25eb775362f223a5
MulLadylex <116450914+MulLadylex@users.noreply.github.com>
2023-05-13 18:09:51 +0800

    完成版

--- src/models/Block.js
+++ src/models/Block.js
@@ -4,21 +4,35 @@ export const DIFFICULTY = 2
 
 class Block {
   // 1. 完成构造函数及其参数
-
-
-  isValid() {}
-
-  setNonce(nonce) {}
-  
-  
-
-  constructor(symbol,previousHash,index,hash) {
+  constructor(symbol,previousHash,height,hash) {
     this.symbol=symbol
     this.previousHash=previousHash
-    this.index=index
+    this.height=height
     this.hash=hash
   }
-}
 
+  /**
+   * 验证数据结构有效
+   * 区块头的哈希值小于目标难度
+   * 第一个交易是coinbase交易
+   * @returns 该区块合法
+   */
+  isValid() {
+    const preHash = this.hash.slice(0,DIFFICULTY)
+    let str = "0"
+    // console.log(str.repeat(2))
+    if (preHash <= str.repeat(DIFFICULTY)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  setNonce(nonce) {
+    this.hash = sha256(nonce + sha256(new Date().getTime().toString()).toString()).toString()
+  }
+  
+  
+}
 export default Block
 
