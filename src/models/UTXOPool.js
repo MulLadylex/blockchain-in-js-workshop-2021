42883e7837c2b4564973ad5b98fff8e364a497a9
MulLadylex <116450914+MulLadylex@users.noreply.github.com>
2023-06-01 23:11:30 +0800

    Update UTXOPool.js
    handTransaction方法的实现isValidTransaction方法的实现

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -6,7 +6,21 @@ class UTXOPool {
   }
 
   // 处理交易函数
-  handleTransaction() {}
+  /**
+   * trx：交易
+   * 判断trx对象中的from在当前UTXO池中
+   * 更新UTXO池
+   */
+  handleTransaction(trx) {
+    if (this.isValidTransaction(trx.from, trx.amount)) {
+      if (!(trx.from in this.utxos)) {
+        throw new Error("Invalid transaction: Input UTXO not found")
+      } else {
+        this.utxos[trx.from].amount -= trx.amount
+        this.addUTXO(trx.to, trx.amount)
+      }
+    }
+  }
 
   // 验证交易合法性
   
@@ -28,7 +42,15 @@ class UTXOPool {
   clone() {
     return this.utxos
   }
-  isValidTransaction() {}
+
+  //  验证交易发起者的UTXO能够支付交易
+  isValidTransaction(utxoId, amount) {
+    if (this.utxos[utxoId] != undefined && this.utxos[utxoId].amount >= amount) {
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 export default UTXOPool
