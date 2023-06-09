e5c58efe424278b28a9352f651df91c8b080e218
MulLadylex <116450914+MulLadylex@users.noreply.github.com>
2023-06-09 17:21:26 +0800

    Update Transaction.js
    实现交易手续费

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,11 +1,12 @@
 import sha256 from 'crypto-js/sha256.js'
 
 class Transaction {
-  constructor(from, to, amount) {
+  constructor(from, to, amount, fee) {
     this.from = from
     this.to = to
     this.amount = amount
     this.timestamp = Date.now()
+    this.fee = fee  
     this.hash = this._calculateHash()
   }
 
@@ -17,7 +18,7 @@ class Transaction {
   // 计算交易 hash 的摘要函数
   //签名过程
   _calculateHash() {
-    return sha256(this.from + this.to + this.amount + this.timestamp).toString()
+    return sha256(this.from + this.to + this.amount + this.fee + this.timestamp).toString()
   }
 }
 
